6d1fd1a1346282d9b7d09f8e18b6ba8f45fb9944
Michael Gebetsroither <michael.geb@gmx.at>
2007-05-26 02:45:41 +0200

    splitting out compiletime sin/cos implementation

--- fft.hpp
+++ fft.hpp
@@ -1,66 +1,21 @@
 #ifndef ___fft_hpp___
 #define ___fft_hpp___
 
+#ifdef TEST_COMPILE
+#include "math.hpp"
+#else
+#include <gebi/math.hpp>
+#endif
+
 #include <loki/Typelist.h>
 
 // license: public domain?
 // FFT implementation from:
 // http://www.ddj.com/dept/cpp/199500857
-// http://www.ddj.com/dept/cpp/199702312?pgno=1
+// http://www.ddj.com/dept/cpp/199702312
 
 namespace gebi
 {
-
-const long double M_PI = 3.1415926535897931;
-
-// static Sin/Cos implementation {{{
-template<unsigned M, unsigned N, unsigned B, unsigned A>
-struct SinCosSeries {
-   static double value() {
-      return 1-(A*M_PI/B)*(A*M_PI/B)/M/(M+1)
-               *SinCosSeries<M+2,N,B,A>::value();
-   }
-};
-
-template<unsigned N, unsigned B, unsigned A>
-struct SinCosSeries<N,N,B,A> {
-   static double value() { return 1.; }
-};
-
-template<unsigned B, unsigned A, typename T = double>
-struct Sin;
-
-
-template<unsigned B, unsigned A>
-struct Sin<B,A,float> {
-   static float value() {
-      return (A*M_PI/B)*SinCosSeries<2,24,B,A>::value();
-   }
-};
-template<unsigned B, unsigned A>
-struct Sin<B,A,double> {
-   static double value() {
-      return (A*M_PI/B)*SinCosSeries<2,34,B,A>::value();
-   }
-};
-
-template<unsigned B, unsigned A, typename T = double>
-struct Cos;
-
-
-template<unsigned B, unsigned A>
-struct Cos<B,A,float> {
-   static float value() {
-      return SinCosSeries<1,23,B,A>::value();
-   }
-};
-template<unsigned B, unsigned A>
-struct Cos<B,A,double> {
-   static double value() {
-      return SinCosSeries<1,33,B,A>::value();
-   }
-};
-// }}}
 
 template<typename T>
 void scramble(T* data, unsigned nn)

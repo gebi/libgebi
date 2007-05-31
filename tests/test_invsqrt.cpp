e27a60d93973384751b487999722625cc82bb963
Michael Gebetsroither <michael.geb@gmx.at>
2007-05-31 19:17:18 +0200

    test_invsqrt: the absolut last check for this mess

--- tests/test_invsqrt.cpp
+++ tests/test_invsqrt.cpp
@@ -3,19 +3,24 @@
 #include <invsqrt.h>
 using namespace gebi;
 
-#include <cmath>
-#include <iostream>
-#include <string>
-using namespace std;
 
-#include <unistd.h>
-
-BOOST_AUTO_TEST_CASE(InvSqrtTest)
-{   
+BOOST_AUTO_TEST_CASE(InvSqrtFloatTest)
+{
     float invsqrt_float = invsqrt(1.5f);
+    if( ! (invsqrt_float <= 0.815361918f &&
+           invsqrt_float >= 0.815361856f)) {
+        BOOST_CHECK_EQUAL(invsqrt_float, 0.815361918);
+        BOOST_CHECK_EQUAL(invsqrt_float, 0.815361856);
+    }
+}
+
+BOOST_AUTO_TEST_CASE(InvSqrtDoubleTest)
+{
     double invsqrt_double = invsqrt((double)1.5);
-    BOOST_CHECK_MESSAGE(invsqrt_float == 0.815361917f || 
-                        invsqrt_float == 0.815361857f, invsqrt_float);
-    BOOST_CHECK_MESSAGE(invsqrt_double == 0.81534821186704765 ||
-                        invsqrt_double == 0.81534821186704765, invsqrt_double);
+    if( ! (invsqrt_double <= 0.81534821186704776L &&
+           invsqrt_double >= 0.81534821186704764L)) {
+        BOOST_CHECK_EQUAL(invsqrt_double, 0.81534821186704776);
+        BOOST_CHECK_EQUAL(invsqrt_double, 0.81534821186704764);
+
+    }
 }
